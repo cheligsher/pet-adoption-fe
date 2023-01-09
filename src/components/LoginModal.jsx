6839d59cbab5e1829a4194e3be743027858d743a
cheligsher <cheligsher@gmail.com>
2023-01-09 18:07:45 +0200

    got rid of old styling

--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -47,32 +47,6 @@ function LoginModal({ showLogin, handleLoginClose }) {
               }/>
             </Form.Group>
           </Form>
-          {/* <div className="d-flex justify-content-evenly">
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              name="email"
-              placeholder="john@doe.com"
-              className="flex-grow-1 ms-3"
-              onChange={(e) =>
-                setUserDetails({ ...userDetails, email: e.target.value })
-              }
-              value={userDetails.email}
-            />
-          </div>
-          <div className="d-flex justify-content-evenly">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              name="password"
-              placeholder="Must be more than 6 char long"
-              className="flex-grow-1 ms-3"
-              onChange={(e) =>
-                setUserDetails({ ...userDetails, password: e.target.value })
-              }
-              value={userDetails.password}
-            />
-          </div> */}
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleLoginClose}>
--- src/components/ProfileForm.jsx
+++ src/components/ProfileForm.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import AppContext from "../context/AppContext";
-import Form from 'react-bootstrap/Form';
+import Form from "react-bootstrap/Form";
 
 function ProfileForm() {
   const { userDetails } = useContext(AppContext);
@@ -12,24 +12,25 @@ function ProfileForm() {
   return (
     <div>
       <Form>
-        <div className="d-flex flex-row justify-content-between"><Form.Group className="mb-3 flex-fill" controlId="formBasicText">
-          <Form.Label>First name</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Your first name"
-            value={firstName}
-          />
-        </Form.Group>
+        <div className="d-flex flex-row justify-content-between">
+          <Form.Group className="mb-3 flex-fill" controlId="formBasicText">
+            <Form.Label>First name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Your first name"
+              value={firstName}
+            />
+          </Form.Group>
 
-        <Form.Group className="mb-3 flex-fill" controlId="formBasicText">
-          <Form.Label>Last name</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Your last name"
-            value={lastName}
-          />
-        </Form.Group></div>
-        
+          <Form.Group className="mb-3 flex-fill" controlId="formBasicText">
+            <Form.Label>Last name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Your last name"
+              value={lastName}
+            />
+          </Form.Group>
+        </div>
 
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
@@ -54,82 +55,10 @@ function ProfileForm() {
           <Form.Control type="password" placeholder="Your new password" />
         </Form.Group>
 
-        <button className="btn btn-dark me-5 mt-3">Update profile</button>
+        <button className="btn btn-dark me-5 mt-3" onClick={handleSubmit}>
+          Update profile
+        </button>
       </Form>
-
-      {/* <form
-        className="w-50 border border-dark p-3 mx-auto mt-4"
-        onSubmit={handleSubmit}
-      >
-        <div className="d-flex justify-content-evenly">
-          <label htmlFor="firstName">First name</label>
-          <input
-            type="text"
-            name="firstName"
-            value={firstName}
-            placeholder="John"
-            className="flex-grow-1 ms-3"
-          />
-        </div>
-        <div className="d-flex justify-content-evenly">
-          <label htmlFor="lastName">Last name</label>
-          <input
-            type="text"
-            name="lastName"
-            value={lastName}
-            placeholder="Doe"
-            className="flex-grow-1 ms-3"
-          />
-        </div>
-        <div className="d-flex justify-content-evenly">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            name="email"
-            value={email}
-            placeholder="john@doe.com"
-            className="flex-grow-1 ms-3"
-          />
-        </div>
-        <div className="d-flex justify-content-evenly">
-          <label htmlFor="phoneNumber">Phone number</label>
-          <input
-            type="number"
-            name="phoneNumber"
-            value={phone}
-            placeholder="0123456789"
-            className="flex-grow-1 ms-3"
-          />
-        </div>
-        <div className="d-flex justify-content-evenly">
-          <label htmlFor="password">Old password</label>
-          <input
-            type="password"
-            name="password"
-            placeholder="Must be more than 6 char long"
-            className="flex-grow-1 ms-3"
-          />
-        </div>
-        <div className="d-flex justify-content-evenly">
-          <label htmlFor="password">New password</label>
-          <input
-            type="password"
-            name="password"
-            placeholder="Must be more than 6 char long"
-            className="flex-grow-1 ms-3"
-          />
-        </div>
-        <div className="d-flex justify-content-evenly">
-          <label htmlFor="bio">Short bio</label>
-          <textarea
-            name="bio"
-            placeholder="Write a bit about yourself..."
-            rows={3}
-            className="flex-grow-1 ms-3"
-          />
-        </div>
-        <button className="btn btn-dark me-5 mt-3">Update profile</button>
-      </form> */}
     </div>
   );
 }
