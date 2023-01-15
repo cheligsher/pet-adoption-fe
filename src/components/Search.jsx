ba1d3d333f542fe8fefa4f6e9b498d7cb3e3aec7
cheligsher <cheligsher@gmail.com>
2023-01-15 18:52:27 +0200

    added basic search by breed

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import SearchBar from "./SearchBar";
 
 function Search() {
+  const [query, setQuery] = useState("");
+  const [petList, setPetList] = useState([]);
+
+  const getAllPets = async () => {
+    try {
+      const allPets = await axios.get("http://localhost:8080/pet");
+      setPetList(allPets.data);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getAllPets();
+  }, []);
+
   return (
     <div className="w-75 text-center mx-auto mt-5">
       <h1 className="display-4 mb-3">
@@ -10,9 +27,28 @@ function Search() {
           <b>Adopt A Pet</b>
         </i>
       </h1>
-      <SearchBar />
+      {/* <SearchBar /> */}
+      <form className="d-flex my-2" role="search">
+        <input
+          className="form-control me-2"
+          type="search"
+          placeholder="Search pet by breed..."
+          onChange={(e)=> setQuery(e.target.value)}
+        />
+        <button className=" btn-outline-dark text-light" type="submit">
+          Search
+        </button>
+      </form>
       <ul className="list-group list-group-flush">
-        <li className="list-group-item">Search results</li>
+        {petList.length &&
+          petList.filter((pet)=> pet.breed.toLowerCase().includes(query))
+          .map((pet) => {
+            return(
+            <li key={pet._id} className="list-group-item">
+              {pet.breed}
+            </li>
+            )
+          })}
       </ul>
     </div>
   );
