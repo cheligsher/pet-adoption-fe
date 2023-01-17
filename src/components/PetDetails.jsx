066054fa5bc3ea7f1b23accbfbe8f75a581b1836
cheligsher <cheligsher@gmail.com>
2023-01-17 09:44:55 +0200

    updated progress

--- src/components/PetDetails.jsx
+++ src/components/PetDetails.jsx
@@ -32,26 +32,26 @@ function PetDetails({handleClose, show, selectedPet}) {
         <Modal.Body>
           {pet && (
             <div id="pet-details d-flex">
-              <img src={pet?.picture} alt="" id="pet-picture" />
+              <img src={pet.picture} alt="" id="pet-picture" />
               <div className="d-flex flex-row justify-content-evenly mt-3">
 
-              <div>Name: {pet?.name}</div>
-              <div>Status: {pet?.adoptionStatus}</div>
+              <div>Name: {pet.name}</div>
+              <div>Status: {pet.adoptionStatus}</div>
               </div>
               <div className="d-flex flex-row justify-content-evenly">
 
-              <div>Type: {pet?.type}</div>
-              <div>Breed: {pet?.breed}</div>
+              <div>Type: {pet.type}</div>
+              <div>Breed: {pet.breed}</div>
               </div> 
               <div className="d-flex flex-row justify-content-evenly">
 
-              <div>Height: {pet?.height} CM</div>
-              <div>Weight: {pet?.weight} KG</div>
+              <div>Height: {pet.height} CM</div>
+              <div>Weight: {pet.weight} KG</div>
               </div>
-              <div>Colour: {pet?.color}</div>
-              <div>Hypoallergenic: {pet?.hypoallergenic}</div>
-              <div>Dietary Restrictions: {pet?.dietary}</div>
-              <div>Bio: {pet?.bio}</div>
+              <div>Colour: {pet.color}</div>
+              <div>Hypoallergenic: {pet.hypoallergenic}</div>
+              <div>Dietary Restrictions: {pet.dietary}</div>
+              <div>Bio: {pet.bio}</div>
             </div>
           )}
         </Modal.Body>
