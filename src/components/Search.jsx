d994c1c47ac384402e518b2e836cd88fbbb0859f
cheligsher <cheligsher@gmail.com>
2023-01-30 12:33:34 +0200

    updated basic search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,32 +5,32 @@ import "../styles/main.css"
 import { toast } from 'react-toastify';
 
 function Search() {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState("Dog");
   const [petList, setPetList] = useState([]);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   const [show, setShow] = useState(false);
   const [selectedPet, setSelectedPet] = useState("")
-
-  const getAllPets = async () => {
+  
+  const getAllPets = async (e) => {
+    e.preventDefault()
     try {
-      const allPets = await axios.get(`http://localhost:8080/pet?${query}`);
+      const allPets = await axios.get(`http://localhost:8080/pet/search/${query}`);
       setPetList(allPets.data);
     } catch (err) {
       toast.error(err.message);
     }
   };
 
-  useEffect(() => {
-    getAllPets();
-  }, [query]);
+  const handleSelect = (e) => {
+    setQuery(e.target.value)
+  }
 
   const handleShowDetails = (pet) => {
     handleShow()
     setSelectedPet(pet)
    };
 
-  // when search, click on pet => navigate to pet page using _id
   return (
     <div className="w-75 text-center mx-auto mt-5 background p-3">
       <h1 className="display-4 mb-3">
@@ -39,19 +39,17 @@ function Search() {
           <b>Adopt A Pet</b>
         </i>
       </h1>
-      {/* <SearchBar /> */}
-      <form className="d-flex my-2" role="search">
-        <input
-          className="form-control me-2"
-          type="search"
-          placeholder="Search pet by breed..."
-          onChange={(e)=> setQuery(e.target.value)}
-        />
+      <form className="d-flex my-2" role="search" onSubmit={getAllPets}>
+        <select onChange={handleSelect}>
+          <option value="Dog">Dog</option>
+          <option value="Cat">Cat</option>
+          <option value="Other">Other</option>
+        </select>
+        <button type="submit">Search</button>
       </form>
       <ul className="list-group list-group-flush overflow-y">
-        {petList.length &&
+        {petList.length>0 &&
           petList
-          .filter((pet)=> pet.breed.toLowerCase().includes(query))
           .map((pet) => {
             return(
             <li key={pet._id} className="list-group-item cursor-pointer transparent" onClick={()=>handleShowDetails(pet)}>
